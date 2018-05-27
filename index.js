4adcf586e63493a26dd4c74279db774041a48539
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-26 20:17:30 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 /* eslint-disable no-magic-numbers */
-import length from "@unction/length"
-
-export default function first (orderedList: ArrayType | string): mixed {
-  return orderedList[length(orderedList) - 1]
+import length from "@unction/length";
+export default function first(orderedList) {
+  return orderedList[length(orderedList) - 1];
 }
