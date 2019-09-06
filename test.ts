db415d173d4a3a1dd589d1499c23384586e8019b
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-09-06 14:56:47 -0700

    The transformer didnt fix this

--- test.ts
+++ test.ts
@@ -1,10 +1,10 @@
 /* eslint-disable no-magic-numbers */
 import last from "./index";
 
-test(() => {
+test("works", () => {
   expect(last([1, 2, 3])).toEqual(3);
 });
 
-test(() => {
+test("works", () => {
   expect(last("abc")).toEqual("c");
 });
