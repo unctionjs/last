9304bdcd24eb33978f571dbb901f1944cdc21926
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2020-11-29 09:49:50 -0800

    Removing more bad types

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 ![Stability][BADGE_STABILITY]
 ![Dependencies][BADGE_DEPENDENCY]
 
-> OrderedArray<A> | Set<A> | RecordType<unknown, A> | string => A
+> OrderedArray<A> | Set<A> | Record<string | number | symbol, B> | Map<B, A> | string => A
 
 Returns the last item of an ordered list.
 
--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 /* eslint-disable no-magic-numbers */
 import length from "@unction/length";
 
-export default function last<A> (orderedList: OrderedArray<A> | Set<A> | RecordType<unknown, A> | string): A {
+export default function last<A> (orderedList: OrderedArray<A> | Set<A> | Record<string | number | symbol, B> | Map<B, A> | string): A {
   return orderedList[length(orderedList) - 1];
 }
